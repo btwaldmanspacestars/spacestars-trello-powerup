7450cf55a9e006f2f66ecab67fd27f9100248943
btwaldmanspacestars <bwaldman@callspacestars.com>
2026-07-19 14:13:44 -0400

    Update client.js

--- client.js
+++ client.js
@@ -3,7 +3,7 @@ var FORM_URL = 'form.html';
 TrelloPowerUp.initialize({
   'card-buttons': function (t, options) {
     return [{
-      icon: 'https://cdn.glitch.com/1b42d7fe-bda8-4af8-a6c8-eff0cea9e08a%2Frocket-ship.png?1494946700421',
+      icon: 'icon.svg',
       text: 'Assign Task',
       condition: 'always',
       callback: function (t) {
