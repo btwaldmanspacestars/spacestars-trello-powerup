717b00bf27cc6d12a42fbacd40830b875f200510
btwaldmanspacestars <bwaldman@callspacestars.com>
2026-07-19 13:48:59 -0400

    Update client.js

--- client.js
+++ client.js
@@ -1,15 +1,16 @@
-var FORM_URL = 'https://keen-klepon-18a58d.netlify.app/form.html';
+var FORM_URL = 'form.html';
 
 TrelloPowerUp.initialize({
-  'card-buttons': function(t, options) {
+  'card-buttons': function (t, options) {
     return [{
       icon: 'https://cdn.glitch.com/1b42d7fe-bda8-4af8-a6c8-eff0cea9e08a%2Frocket-ship.png?1494946700421',
       text: 'Assign Task',
-      callback: function(t) {
+      condition: 'always',
+      callback: function (t) {
         return t.popup({
-          title: 'Assign Task',
+          title: 'Assign Task — SpaceStars',
           url: FORM_URL,
-          height: 430
+          height: 460
         });
       }
     }];
